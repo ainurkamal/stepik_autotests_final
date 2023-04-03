d3411a84aec957e9027a1e92d3b94fdbd7966ad7
ainurkamal <ainurkamaleev@gmail.com>
2023-04-03 13:44:45 +0300

    Add MainPage class to main_page.py
    * MainPage inherits BasePage from base_page.py
    * Add to MainPage go_to_login_page() method
    * Updated test_guest_can_go_to_login() method
      in test_main_page.py

--- final_project/test_main_page.py
+++ final_project/test_main_page.py
@@ -1,7 +1,11 @@
+from .pages.main_page import MainPage
 from selenium.webdriver.common.by import By
 
+link = "http://selenium1py.pythonanywhere.com/"
+
+
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
+    page = MainPage(browser, link)
     browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page.open()
+    page.go_to_login_page()
